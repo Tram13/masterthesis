eadad8d2739a8bfb9f9eca9fbe03a9c87fa168e0
ardjonge <arnoud.dejonge@ugent.be>
2023-02-15 16:24:41 +0100

    added a basic sentiment analysis in sentiment_analysis.py

---
+++ src/NLP/Models/sentiment_analysis.py
@@ -0,0 +1,24 @@
+from transformers import pipeline
+
+
+class BasicSentimentAnalysis:
+
+    def __init__(self) -> None:
+        self.pipeline = pipeline("sentiment-analysis")
+
+    def get_sentiment(self, text: list[str]) -> list[dict]:
+        return self.pipeline(text)
+
+
+if __name__ == '__main__':
+    test_model = BasicSentimentAnalysis()
+
+    t1 = ["Please read the analysis.", "You'll be amazed."]
+    t2 = ["This restaurant sucks.", "It has the worst staff and terrible food.", "Only Jonny liked it."]
+
+    output = test_model.get_sentiment(t1)
+    print(output)
+
+    output = test_model.get_sentiment(t2)
+    print(output)
+
