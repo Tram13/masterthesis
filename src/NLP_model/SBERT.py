111ac9e0dabdc37b0b6ddacaeae3a00bf5fc49bd
ardjonge <arnoud.dejonge@ugent.be>
2023-02-15 14:52:12 +0100

    Added BERT.py (WIP) and SBERT.py updated requirements.txt

---
+++ src/NLP_model/SBERT.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+
+class SentenceBERT:
+    # https://www.sbert.net/docs/pretrained_models.html
+    def __init__(self, model_name: str = 'all-mpnet-base-v2') -> None:
+        self.model = SentenceTransformer(model_name)
+
+    def get_features(self, sentences: list[str]) -> np.ndarray:
+        return self.model.encode(sentences=sentences)
+
+
+if __name__ == '__main__':
+    # Our sentences we like to encode
+    s = ['This framework generates embeddings for each input sentence',
+         'Sentences are passed as a list of string.',
+         'The quick brown fox jumps over the lazy dog.']
+    # Model to use
+    SBERTmodel = SentenceBERT()
+    embedding = SBERTmodel.get_features(sentences=s)
+    print(embedding)
+    print(embedding.shape)
