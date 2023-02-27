736b4b5927b98c25d6ea024bd4271e3719be1fe4
Tram13 <arno.vermote@hotmail.com>
2023-02-27 15:43:17 +0100

    fixed for python3.9

--- src/tools/config_parser.py
+++ src/tools/config_parser.py
@@ -1,24 +1,31 @@
 from configparser import ConfigParser as BuiltInConfPars
 import os
 from pathlib import Path
+from typing import Union
 
 
 # Extension of built-in Config Parser of Python
 class ConfigParser:
     # Location of the .ini file that contains all configuration variables
-    PATH: os.PathLike = Path("../config.ini")  # Default location
 
-    CP_INSTANCE = BuiltInConfPars()
-    CP_INSTANCE.read(PATH)
-    type_to_function_map = {
-        bool: CP_INSTANCE.getboolean,
-        int: CP_INSTANCE.getint,
-        float: CP_INSTANCE.getfloat,
-        str: CP_INSTANCE.get
-    }
+    def __init__(self):
+        self.PATH: os.PathLike = Path("../config.ini")  # Default location
+        self.CP_INSTANCE = BuiltInConfPars()
+        self.CP_INSTANCE.read(self.PATH)
+        self.type_to_function_map = {
+            bool: self.CP_INSTANCE.getboolean,
+            int: self.CP_INSTANCE.getint,
+            float: self.CP_INSTANCE.getfloat,
+            str: self.CP_INSTANCE.get
+        }
 
-    @staticmethod
-    def get_value(section: str, key: str, return_type: type[bool | int | float | str] = str):
+    def get_value(self, section: str, key: str, return_type: type[Union[bool, int, float, str]] = str):
+        self._assert_config_path()
         # Get the read-function that matches the desired return type
-        type_function = ConfigParser.type_to_function_map.get(return_type, ConfigParser.CP_INSTANCE.get)
+        type_function = self.type_to_function_map.get(return_type, self.CP_INSTANCE.get)
         return type_function(section, key)
+
+    def _assert_config_path(self):
+        self.PATH = Path("../config.ini")  # Needs to be updated for Jupyter notebooks
+        if not (os.path.exists(self.PATH) and os.path.isfile(self.PATH)):
+            raise FileNotFoundError(f"config.ini not found in {self.PATH}")
