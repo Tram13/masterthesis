f9a18b889af424cbb56b32eecf631df2d28a50b0
Tram13 <arno.vermote@hotmail.com>
2023-02-27 15:51:42 +0100

    created basic main() function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 from src.data.data_reader import DataReader
+import torch
 
-print("hello world")
-businesses, reviews, tips, users = DataReader().read_data()
+def main():
+    print("hello world")
+    businesses, reviews, tips, users = DataReader().read_data()
+    print(torch.cuda.is_available())
 
+
+if __name__ == '__main__':
+    main()
