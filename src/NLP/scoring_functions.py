0e9a414a546067b7c4887d3ca313c7f4c62c4265
ardjonge <arnoud.dejonge@ugent.be>
2023-03-03 21:31:47 +0100

    Added custom scoring function for online BERTopic in scoring_functions.py

--- src/NLP/scoring_functions.py
+++ src/NLP/scoring_functions.py
@@ -1,6 +1,18 @@
 import numpy as np
 
 
+def online_bertopic_scoring_func(col, total_amount_topics):
+    output = np.zeros(total_amount_topics)
+
+    # add to main topic: sentiment_score
+    np.add.at(output, col[0], col[1] * col[2])
+
+    # we can not normalize between 0 and 1 because of full negative reviews
+
+    # normalize output by dividing by amount of sentences
+    return output / len(col[0])
+
+
 def bertopic_scoring_func(col, total_amount_topics, weight_main_topics=0.75):
     output = np.zeros(total_amount_topics)
 
