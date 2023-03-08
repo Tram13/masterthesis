c637738b256a9e024f4c28583367d21b43a90900
ardjonge <arnoud.dejonge@ugent.be>
2023-03-08 15:47:17 +0100

    WIP better user profiles

--- src/main.py
+++ src/main.py
@@ -11,6 +11,35 @@ from src.data.data_preparer import DataPreparer
 from src.data.data_reader import DataReader
 from src.tools.config_parser import ConfigParser
 from src.user_profile_creation import calculate_basic_user_profiles
+from multiprocessing import Pool
+
+
+def pool_func(param):
+    return calculate_profile_by_user_pool(param[0], param[1])
+
+
+def calculate_profile_by_user_pool(reviews: pd.Series, user_id: str, use_cache=True, save_in_cache=True):
+    current_save_dir = Path(ConfigParser().get_value('data', 'user_profiles_path'))
+    if not current_save_dir.is_dir():
+        current_save_dir.mkdir()
+
+    save_path = current_save_dir.joinpath(f"{user_id}.parquet")
+
+    if use_cache:
+        try:
+            user_profiles = pd.read_parquet(save_path, engine='fastparquet')
+            return user_profiles
+        except OSError:
+            pass
+
+    scores = create_scores_from_online_model(reviews, use_cache=False, save_in_cache=False, verbose=False)
+    user_profiles = scores.aggregate(['mean'], axis=0)
+    user_profiles.columns = [str(x) for x in user_profiles.columns]
+
+    if save_in_cache:
+        user_profiles.to_parquet(save_path, engine='fastparquet')
+
+    return user_profiles
 
 
 def calculate_profile_by_user(reviews: pd.DataFrame, user_id: str, use_cache=True, save_in_cache=True):
@@ -51,8 +80,11 @@ def main():
 
     logging.basicConfig(level=logging.WARNING)
 
-    for user in tqdm(reviews['user_id'].unique()):
-        calculate_profile_by_user(reviews, user)
+    # too slow
+    # reviews = [(group['text'], name) for name, group in reviews.groupby('user_id')]
+
+    # for review, user in tqdm(reviews):
+    #   calculate_profile_by_user_pool(review, user)
 
     # to memory heavy code:
     # scores = create_scores_from_online_model(reviews['text'], use_cache=True, save_in_cache=False)
