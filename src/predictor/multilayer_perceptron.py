49530eca8630cb2cb4b1466d5e0fa4434729115f
Tram13 <arno.vermote@hotmail.com>
2023-03-08 15:54:39 +0100

    Added a base model for ML Still need to figure out the optimal architecture

--- src/predictor/multilayer_perceptron.py
+++ src/predictor/multilayer_perceptron.py
@@ -5,37 +5,24 @@ import torch.optim
 class MultiLayerPerceptronPredictor(nn.Module):
     def __init__(self, input_size: int, output_size: int):
         super(MultiLayerPerceptronPredictor, self).__init__()
-        self.l1 = nn.Linear(input_size, 5)
-        self.relu = nn.ReLU()
-        self.L2 = nn.Linear(5, output_size)
+        self.flatten = nn.Flatten()
+        # Definition of netwok architecture
+        self.linear_stack = nn.Sequential(
+            nn.Linear(input_size, input_size // 2),
+            nn.Sigmoid(),
+            nn.Linear(input_size // 2, input_size // 4),
+            nn.ReLU(),
+            nn.Linear(input_size // 4, input_size // 8),
+            nn.Sigmoid(),
+            nn.Linear(input_size // 8, input_size // 16),
+            nn.Sigmoid(),
+            nn.Linear(input_size // 16, output_size)
+        )
+
         # Run on GPU is available
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.to(device)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.to(self.device)
 
     def forward(self, x):
-        output = self.l1(x)
-        output = self.relu(output)
-        output = self.l2(output)
-        return output
-
-
-if __name__ == '__main__':
-    model = MultiLayerPerceptronPredictor(80, 1)
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
-    epochs = 1500
-
-    costval = []
-    for j in range(epochs):
-        for i, (x_train, y_train) in enumerate(dataloader):    #prediction
-            y_pred = model(x_train)
-            # calculating loss
-            cost = criterion(y_pred, y_train.reshape(-1, 1))
-            # backprop
-            optimizer.zero_grad()
-            cost.backward()
-            optimizer.step()
-        if j % 50 == 0:
-            print(cost)
-            costval.append(cost)
-
+        x = self.flatten(x)
+        return self.linear_stack(x)
