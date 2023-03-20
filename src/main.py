bbaca45a6b943fd9ef73865a12c44307e76fc2e2
Tram13 <arno.vermote@hotmail.com>
2023-03-20 01:28:57 +0100

    run all scores

--- src/main.py
+++ src/main.py
@@ -39,13 +39,10 @@ def main_user_profile():
 
     amount_of_batches = 10
     for index, batch in enumerate(tqdm(np.array_split(reviews, amount_of_batches), desc="Score Batches")):
-        if index >= 5:
-            print()
-            scores = create_scores_from_online_model(batch['text'], use_cache=False, save_in_cache=False, early_return=True)
-            scores.columns = [str(x) for x in scores.columns]
-            scores.to_parquet(Path(cache_path, f"score_part_{index}.parquet"), engine='fastparquet')
-
-    exit(0)
+        print()
+        scores = create_scores_from_online_model(batch['text'], use_cache=False, save_in_cache=False, early_return=True)
+        scores.columns = [str(x) for x in scores.columns]
+        scores.to_parquet(Path(cache_path, f"score_part_{index}.parquet"), engine='fastparquet')
 
     scores = pd.read_parquet(Path(cache_path, f"score_part_{0}.parquet"), engine='fastparquet')
     for index in range(1, amount_of_batches):
