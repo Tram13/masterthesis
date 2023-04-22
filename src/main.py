913d274d26fd05968bb01bac2db973acb1847dd7
arvermot <arvermot.vermote@ugent.be>
2023-04-22 14:15:42 +0200

    new name when not using business profile

--- src/main.py
+++ src/main.py
@@ -16,26 +16,43 @@ from tools.profiles_manager import ProfilesManager
 def main():
     businesses, reviews, tips = DataReader().read_data()
 
-    logging.info("Starting training: with user profiles and business profiles")
+    # logging.info("Starting training: with user profiles and business profiles")
+    # for user_profiles_name in tqdm(ProfilesManager().get_user_profiles_names(), desc="User Profiles"):
+    #     for business_profiles_name in tqdm(ProfilesManager().get_business_profiles_names(), desc="Business Profiles"):
+    #
+    #         user_profiles = ProfilesManager().get_user_profiles(user_profiles_name)
+    #         business_profiles = ProfilesManager().get_business_profiles(business_profiles_name)
+    #
+    #         train_test_data = DataPreparer.get_train_test_validate(businesses, reviews, tips, user_profiles, business_profiles)
+    #
+    #         model = MultiLayerPerceptronPredictor(input_size=train_test_data[0].columns.size, output_size=1)
+    #         # Check if model already exists
+    #         save_dir = ConfigParser().get_value('predictor_model', 'model_dir')
+    #         if len([file.name for file in os.scandir(Path(save_dir)) if user_profiles_name in file.name and business_profiles_name in file.name]) == 0:  # if not found
+    #             optimizer = optim.Adam(model.parameters(), lr=0.002)
+    #
+    #             nn_trainer = NeuralNetworkTrainer(user_profiles_name, business_profiles_name, *train_test_data)
+    #             model, optimizer = nn_trainer.train(model, optimizer, epochs=100, save_to_disk=True, verbose=False)
+    #             model.plot_loss_progress(save_location=Path("predictor", f"loss_mlp_{user_profiles_name}_{business_profiles_name}.png"))
+    #         else:
+    #             logging.info(f"Skipped model {model.get_default_save_location()}, another version already exists")
+
     for user_profiles_name in tqdm(ProfilesManager().get_user_profiles_names(), desc="User Profiles"):
-        for business_profiles_name in tqdm(ProfilesManager().get_business_profiles_names(), desc="Business Profiles"):
+        user_profiles = ProfilesManager().get_user_profiles(user_profiles_name)
 
-            user_profiles = ProfilesManager().get_user_profiles(user_profiles_name)
-            business_profiles = ProfilesManager().get_business_profiles(business_profiles_name)
+        train_test_data = DataPreparer.get_train_test_validate(businesses, reviews, tips, user_profiles)
 
-            train_test_data = DataPreparer.get_train_test_validate(businesses, reviews, tips, user_profiles, business_profiles)
+        model = MultiLayerPerceptronPredictor(input_size=train_test_data[0].columns.size, output_size=1)
+        # Check if model already exists
+        save_dir = ConfigParser().get_value('predictor_model', 'model_dir')
+        if len([file.name for file in os.scandir(Path(save_dir)) if user_profiles_name in file.name and "None" in file.name]) == 0:  # if not found
+            optimizer = optim.Adam(model.parameters(), lr=0.002)
 
-            model = MultiLayerPerceptronPredictor(input_size=train_test_data[0].columns.size, output_size=1)
-            # Check if model already exists
-            save_dir = ConfigParser().get_value('predictor_model', 'model_dir')
-            if len([file.name for file in os.scandir(Path(save_dir)) if user_profiles_name in file.name and business_profiles_name in file.name]) == 0:  # if not found
-                optimizer = optim.Adam(model.parameters(), lr=0.002)
-
-                nn_trainer = NeuralNetworkTrainer(user_profiles_name, business_profiles_name, *train_test_data)
-                model, optimizer = nn_trainer.train(model, optimizer, epochs=100, save_to_disk=True, verbose=False)
-                model.plot_loss_progress(save_location=Path("predictor", f"loss_mlp_{user_profiles_name}_{business_profiles_name}.png"))
-            else:
-                logging.info(f"Skipped model {model.get_default_save_location()}, another version already exists")
+            nn_trainer = NeuralNetworkTrainer(user_profiles_name, None, *train_test_data)
+            model, optimizer = nn_trainer.train(model, optimizer, epochs=100, save_to_disk=True, verbose=False)
+            model.plot_loss_progress(save_location=Path("predictor", f"loss_mlp_{user_profiles_name}_None.png"))
+        else:
+            logging.info(f"Skipped model {model.get_default_save_location()}, another version already exists")
 
 
 if __name__ == '__main__':
