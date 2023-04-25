22eb5cf520a1dbb07c55108e968a5e632d9bff4a
arvermot <arvermot.vermote@ugent.be>
2023-04-25 03:00:17 +0200

    Enabled usage of Users dataframe for DataPreparer

--- src/data/data_preparer.py
+++ src/data/data_preparer.py
@@ -6,16 +6,18 @@ from sklearn.model_selection import train_test_split
 class DataPreparer:
 
     @staticmethod
-    def get_train_test_validate(businesses: pd.DataFrame, reviews: pd.DataFrame, tips: pd.DataFrame, user_profiles: pd.DataFrame, business_profiles: pd.DataFrame = None) \
+    def get_train_test_validate(businesses: pd.DataFrame, reviews: pd.DataFrame, tips: pd.DataFrame, users: pd.DataFrame, user_profiles: pd.DataFrame,
+                                business_profiles: pd.DataFrame = None) \
             -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        ml_data = DataPreparer.get_df_for_ml(businesses, reviews, tips, user_profiles, business_profiles).reset_index()
+        ml_data = DataPreparer.get_df_for_ml(businesses, reviews, tips, users, user_profiles, business_profiles).reset_index()
         output_ml = ml_data['stars_normalised']
         input_ml = ml_data.drop(columns=['stars_normalised', 'review_id', 'user_id', 'business_id'])
         input_ml_train, input_ml_test, output_ml_train, output_ml_test = train_test_split(input_ml, output_ml, test_size=0.2)
         return input_ml_train, input_ml_test, output_ml_train, output_ml_test
 
     @staticmethod
-    def get_df_for_ml(businesses: pd.DataFrame, reviews: pd.DataFrame, tips: pd.DataFrame, user_profiles: pd.DataFrame, business_profiles: pd.DataFrame = None) -> pd.DataFrame:
+    def get_df_for_ml(businesses: pd.DataFrame, reviews: pd.DataFrame, tips: pd.DataFrame, users: pd.DataFrame, user_profiles: pd.DataFrame,
+                      business_profiles: pd.DataFrame = None) -> pd.DataFrame:
         """
         Get all combinations of business attributes and user attributes
         :return: A DataFrame where each row represents an input for the ML model
@@ -29,7 +31,7 @@ class DataPreparer:
         user_reviewed_restaurant = reviews[['user_id', 'business_id', 'stars_normalised', *user_profiles.columns]]
         user_reviewed_restaurant = user_reviewed_restaurant.join(businesses, on='business_id')
 
-        # user_reviewed_restaurant = user_reviewed_restaurant.join(users, on='user_id')  # We don't use the users dataset at this moment
+        user_reviewed_restaurant = user_reviewed_restaurant.join(users, on='user_id')
         user_reviewed_restaurant = user_reviewed_restaurant.set_index(['user_id', 'business_id'], append=True)
 
         return user_reviewed_restaurant
