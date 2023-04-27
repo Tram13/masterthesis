c70e6d9538757fd323c530becdf94b45662a0c04
arvermot <arvermot.vermote@ugent.be>
2023-04-27 23:15:57 +0200

    Experiments with new loss functions

---
+++ src/tools/loss_score.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+
+
+class LossScore(nn.Module):
+    def __init__(self):
+        super(LossScore, self).__init__()
+
+    def forward(self, output, target):
+        return self.class_based_loss(output, target)
+
+    def MSE_variation(self, output, target):
+        # Variation on MSE
+        loss = torch.square(target - output)
+        # Higher loss for misprediction of low rated items
+        is_3_or_less_stars_penalty = (target <= 0.5).float() * 0.5
+        return (loss + is_3_or_less_stars_penalty.float()).mean()
+
+    def class_based_loss(self, output, target):
+        loss = torch.abs(target - output)
+        accuracy = (loss > 0.125).float() * 0.5
+        return (loss + accuracy).mean()
+
