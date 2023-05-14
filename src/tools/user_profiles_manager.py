04b2e2daaba60e63ff2fb0e91c2f02cf12602cfd
arvermot <arvermot.vermote@ugent.be>
2023-05-14 20:47:42 +0200

    missing caches

--- src/tools/user_profiles_manager.py
+++ src/tools/user_profiles_manager.py
@@ -52,14 +52,14 @@ class UserProfilesManager:
             approx_amount_top_n=5,
             filter_useful_topics=False
         ).get_build_parameters(),
-        ProfileCreator(
-            model_name="online_model_400top_97.bert",
-            use_sentiment_in_scores=False,
-            approx_mode=True,
-            approx_normalization=True,
-            approx_amount_top_n=5,
-            filter_useful_topics=False
-        ).get_build_parameters(),
+        # ProfileCreator(  # TODO: cache missing
+        #     model_name="online_model_400top_97.bert",
+        #     use_sentiment_in_scores=False,
+        #     approx_mode=True,
+        #     approx_normalization=True,
+        #     approx_amount_top_n=5,
+        #     filter_useful_topics=False
+        # ).get_build_parameters(),
         # ProfileCreator(  # TODO: opnieuw aanzetten
         #     model_name="offline_bertopic_100000.bert",
         #     use_sentiment_in_scores=False,
