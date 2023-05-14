edf66acae7f598ac79e6adb3e37be0c6b9ae7caf
arvermot <arvermot.vermote@ugent.be>
2023-05-14 15:48:14 +0200

    temp removed a profile

--- src/tools/restaurant_profiles_manager.py
+++ src/tools/restaurant_profiles_manager.py
@@ -52,14 +52,14 @@ class RestaurantProfilesManager:
             approx_amount_top_n=5,
             filter_useful_topics=False
         ).get_build_parameters(),
-        ProfileCreator(
-            model_name="offline_bertopic_100000.bert",
-            use_sentiment_in_scores=True,
-            approx_mode=False,
-            approx_normalization=True,
-            approx_amount_top_n=5,
-            filter_useful_topics=False
-        ).get_build_parameters(),
+        # ProfileCreator(  # TODO: opnieuw aanzetten
+        #     model_name="offline_bertopic_100000.bert",
+        #     use_sentiment_in_scores=True,
+        #     approx_mode=False,
+        #     approx_normalization=True,
+        #     approx_amount_top_n=5,
+        #     filter_useful_topics=False
+        # ).get_build_parameters(),
         # ProfileCreator(  # TODO: opnieuw aanzetten
         #     model_name="online_model_400top_97.bert",
         #     use_sentiment_in_scores=False,
