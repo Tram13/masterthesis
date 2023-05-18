1ffd81a887319ca7a895b13d6a96122f6a97e806
arvermot <arvermot.vermote@ugent.be>
2023-05-18 18:21:48 +0200

    WIP: de scores zijn slecht maar ik weet niet waarom. Ik probeer eens een oudere versie nu

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ import logging
 import random
 from pathlib import Path
 
-import pandas as pd
 from torch import optim
 from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 
 from NLP.main_user_profiles import main_user_profile_topic
 from NLP.utils.evaluate_model import evaluate_model
@@ -20,24 +20,22 @@ from predictor.implementations.multilayer_perceptron5 import MultiLayerPerceptro
 from predictor.implementations.multilayer_perceptron6 import MultiLayerPerceptron6Predictor
 from predictor.implementations.multilayer_perceptron7 import MultiLayerPerceptron7Predictor
 from predictor.implementations.multilayer_perceptron8 import MultiLayerPerceptron8Predictor
-from predictor.implementations.random_forest import RandomForest
 from predictor.neural_network_trainer import NeuralNetworkTrainer
 from tools.restaurant_profiles_manager import RestaurantProfilesManager
 from tools.user_profiles_manager import UserProfilesManager
 
 
-def parse_data_train_test(train_data: tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame], test_data: tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame], profile_params: tuple[dict, dict]) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    logging.info("Parsing train set")
-    training_input, training_output = DataPreparer.transform_data(*train_data, *profile_params, profile_size=0.5)  # TODO: in het verslag vermelden!
-    logging.info("Parsing test set")
-    test_input, test_output = DataPreparer.transform_data(*test_data, *profile_params, profile_size=0.7)
-    gc.collect()
-    return training_input, test_input, training_output, test_output
+def single_model_multiprocessed(args):
+    nn_trainer, models, optimizers, index, SUB_EPOCHS, EPOCHS = args
+    model, optimizer = nn_trainer.train(models[index], optimizers[index], sub_epochs=SUB_EPOCHS, save_to_disk=(EPOCHS == 1), verbose=False)
+    models[index] = model
+    optimizers[index] = optimizer
+    logging.info(f"Current loss history: {[f'{val:.3}' for val in model.loss_history[-5:]]}")
 
 
-def main_single_model(train_data, test_data, up_params, rp_params, EPOCHS, SUB_EPOCHS, LR):
+def main_single_model(train_data, test_data, up_params, rp_params, EPOCHS, SUB_EPOCHS, LR, multiprocess: bool = False):
     with tqdm(total=EPOCHS, desc="Epochs", leave=False) as p_bar:
-        train_test_data = parse_data_train_test(train_data, test_data, (up_params, rp_params))
+        train_test_data = DataPreparer.parse_data_train_test(train_data, test_data, (up_params, rp_params), cache_index_if_available=0)
 
         logging.info("Transforming data to DataLoaders")
         nn_trainer = NeuralNetworkTrainer(up_params, rp_params, *train_test_data)
@@ -65,31 +63,72 @@ def main_single_model(train_data, test_data, up_params, rp_params, EPOCHS, SUB_E
             optim.SGD(models[7].parameters(), lr=LR)
         ]
 
-        logging.info("Starting training")
+        # Setting default parameter correctly
+        for model in models:
+            model.optimizer = "SGD"
+            model.lr = LR
 
-        for index, (model, optimizer) in tqdm(enumerate(zip(models, optimizers)), desc="Training models", leave=False):
-            model, optimizer = nn_trainer.train(model, optimizer, sub_epochs=SUB_EPOCHS, save_to_disk=(EPOCHS == 1), verbose=True)
-            models[index] = model
-            optimizers[index] = optimizer
-            logging.info(f"Current loss history: {[f'{val:.3}' for val in model.loss_history[-5:]]}")
+        models.extend((
+            MultiLayerPerceptron5Predictor(input_size=nn_trainer.train_loader.dataset.x_train.shape[1]),
+            MultiLayerPerceptron6Predictor(input_size=nn_trainer.train_loader.dataset.x_train.shape[1]),
+            MultiLayerPerceptron5Predictor(input_size=nn_trainer.train_loader.dataset.x_train.shape[1]),
+            MultiLayerPerceptron6Predictor(input_size=nn_trainer.train_loader.dataset.x_train.shape[1]),
+            MultiLayerPerceptron5Predictor(input_size=nn_trainer.train_loader.dataset.x_train.shape[1]),
+            MultiLayerPerceptron6Predictor(input_size=nn_trainer.train_loader.dataset.x_train.shape[1])
+        ))
+
+        optimizers.extend((
+            optim.Adagrad(models[8].parameters(), lr=LR),
+            optim.Adagrad(models[9].parameters(), lr=LR),
+            optim.Adagrad(models[10].parameters(), lr=0.002),
+            optim.Adagrad(models[11].parameters(), lr=0.002),
+            optim.Adagrad(models[12].parameters(), lr=0.02),
+            optim.Adagrad(models[13].parameters(), lr=0.02),
+        ))
+
+        models[8].lr = LR
+        models[9].lr = LR
+        models[10].lr = 0.002
+        models[11].lr = 0.002
+        models[12].lr = 0.02
+        models[13].lr = 0.02
+
+        logging.info("Starting training")
+        if multiprocess:
+            args = [(nn_trainer, models, optimizers, index, SUB_EPOCHS, EPOCHS) for index in range(len(models))]
+            process_map(single_model_multiprocessed, args, max_workers=5)
+        else:
+            for index, (model, optimizer) in tqdm(enumerate(zip(models, optimizers)), desc="Training models", leave=False):
+                if index == 9:
+                    model, optimizer = nn_trainer.train(model, optimizer, sub_epochs=SUB_EPOCHS, save_to_disk=(EPOCHS == 1), verbose=True)
+                    models[index] = model
+                    optimizers[index] = optimizer
+                    logging.info(f"Current loss history: {[f'{val:.3}' for val in model.loss_history[-5:]]}")
         p_bar.update()
+        logging.info(f"Validation check: {models[11].current_epoch} epochs in models[2]")
 
         for epoch in range(2, EPOCHS + 1):
+            gc.collect()  # Clean up last run
             # Creates new user profiles based on subset of train or test data
-            train_test_data = parse_data_train_test(train_data, test_data, (up_params, rp_params))
+            train_test_data = DataPreparer.parse_data_train_test(train_data, test_data, (up_params, rp_params), cache_index_if_available=epoch-1)
 
             logging.info("Transforming data to DataLoaders")
             nn_trainer = NeuralNetworkTrainer(up_params, rp_params, *train_test_data)
             gc.collect()
 
             logging.info("Starting training")
-            for index, (model, optimizer) in tqdm(enumerate(zip(models, optimizers)), desc="Training models", leave=False):
-                model, optimizer = nn_trainer.train(model, optimizer, sub_epochs=SUB_EPOCHS, save_to_disk=(EPOCHS == epoch), verbose=True)
-                models[index] = model
-                optimizers[index] = optimizer
-                logging.info(f"Current loss history: {[f'{val:.3}' for val in model.loss_history[-5:]]}")
+            if multiprocess:
+                args = [(nn_trainer, models, optimizers, index, SUB_EPOCHS, EPOCHS) for index in range(len(models))]
+                process_map(single_model_multiprocessed, args, max_workers=4)
+            else:
+                for index, (model, optimizer) in tqdm(enumerate(zip(models, optimizers)), desc="Training models", leave=False):
+                    if index == 9:
+                        model, optimizer = nn_trainer.train(model, optimizer, sub_epochs=SUB_EPOCHS, save_to_disk=(EPOCHS == epoch), verbose=True)
+                        models[index] = model
+                        optimizers[index] = optimizer
+                        logging.info(f"Current loss history: {[f'{val:.3}' for val in model.loss_history[-5:]]}")
+            logging.info(f"Validation check: {models[11].current_epoch} epochs in models[2]")
             p_bar.update()
-            gc.collect()
 
         # Save statistics
     for model in models:
@@ -97,12 +136,7 @@ def main_single_model(train_data, test_data, up_params, rp_params, EPOCHS, SUB_E
     gc.collect()
 
 
-def main_all_models():
-    # Parameters
-    EPOCHS = 5
-    SUB_EPOCHS = 30
-    LR = 0.0002
-
+def main_profiles_grid_search_for_nn(EPOCHS: int, SUB_EPOCHS: int, LR: int):
     logging.info("Reading Yelp Dataset")
     train_data, test_data = DataReader().read_data()
     gc.collect()
@@ -180,22 +214,10 @@ def main_evaluate_model(model_name):
     evaluate_model(sentences, model_name, 2, False)
 
 
-def main_random_forest(train_data, test_data, up_params, rp_params):
-    training_input, test_input, training_output, test_output = parse_data_train_test(train_data, test_data, (up_params, rp_params))
-    forest = RandomForest(training_input, test_input, training_output, test_output)
-    logging.info("Fitting Random Forest")
-    forest.train()
-    logging.info("Testing Random Forest")
-    results = forest.validate()
-    logging.info("Random Forest results:")
-    logging.info(f"{results[0]}")
-    logging.info(f"{results[1]}")
-
-
 if __name__ == '__main__':
     # Note: force manual garbage collection is used to save on memory after heavy RAM and I/O instructions
-    EPOCHS = 5
-    SUB_EPOCHS = 30
+    EPOCHS = 10
+    SUB_EPOCHS = 40
     LR = 0.0002
 
     logging.basicConfig(
@@ -210,10 +232,10 @@ if __name__ == '__main__':
     rp_params = RestaurantProfilesManager().get_best()
     gc.collect()
 
-    logging.info("***************** generating batches *****************")
-    main_user_profile_offline_bert()
+    logging.info("***************** alle netwerkconfiguraties proberen *****************")
+    main_single_model(train_data, test_data, up_params, rp_params, EPOCHS, SUB_EPOCHS, LR)
 
-    # logging.info("***************** alle netwerkconfiguraties proberen *****************")
-    # main_single_model(train_data, test_data, up_params, rp_params, EPOCHS, SUB_EPOCHS, LR)
+    # logging.info("***************** splits maken *****************")
+    # DataPreparer.make_nn_caches(train_data, test_data, up_params, rp_params, n=50)
     # logging.info("***************** random forest trainen *****************")
     # main_random_forest(train_data, test_data, up_params, rp_params)
