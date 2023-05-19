dab2bf77bf942112c532d87b4a4a64a132e7a853
arvermot <arvermot.vermote@ugent.be>
2023-05-19 13:36:25 +0200

    better save name

--- src/predictor/implementations/multilayer_perceptron.py
+++ src/predictor/implementations/multilayer_perceptron.py
@@ -53,7 +53,7 @@ class MultiLayerPerceptronPredictor(nn.Module):
         else:
             file_name = ConfigParser().get_value('predictor_model', 'mlp_full_model_name').split('.')
         uuid = datetime.now().strftime("%Y-%m-%d_%Hh%M")
-        return Path(save_dir, f'{file_name[0]}{self.version}_{uuid}__EPOCHS={self.current_epoch}_LOSS={self.loss_history[-1]:.3}_LR={self.lr}.{file_name[1]}')
+        return Path(save_dir, f'{file_name[0]}{self.version}_{uuid}__EPOCHS={self.current_epoch}_LR={self.lr}_LOSS={self.loss_history[-1]:.3}.{file_name[1]}')
 
     @staticmethod
     def get_latest_model_from_default_location(use_inference_model: bool = False) -> Path:  # Only for full model
