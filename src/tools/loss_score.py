7d8ea87b71ee4afb8e62613c8b381c9fc478d239
arvermot <arvermot.vermote@ugent.be>
2023-05-22 02:18:24 +0200

    added extra MSE implementation

--- src/tools/loss_score.py
+++ src/tools/loss_score.py
@@ -8,14 +8,19 @@ class LossScore(nn.Module):
         super(LossScore, self).__init__()
 
     def forward(self, output, target):
-        return self.class_based_loss(output, target)
+        return self.MSE_variation2(output, target)
 
-    def MSE_variation(self, output, target):
+    def MSE_variation2(self, output, target):
         # Variation on MSE
         loss = torch.square(target - output)
         # Higher loss for misprediction of low rated items
         is_3_or_less_stars_penalty = (target <= 0.5).float() * 0.5
         return (loss + is_3_or_less_stars_penalty.float()).mean()
+
+    def MSE_variation1(self, output, target):
+        loss = torch.square(target - output)
+        loss = torch.mul(loss, 5)
+        return loss.mean()
 
     def class_based_loss(self, output, target):
         loss = torch.abs(target - output)
