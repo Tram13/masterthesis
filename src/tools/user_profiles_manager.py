42546b937620e79ca4feb12bd295109112a97ab5
arvermot <arvermot.vermote@ugent.be>
2023-05-23 02:02:01 +0200

    removed TODO's

--- src/tools/user_profiles_manager.py
+++ src/tools/user_profiles_manager.py
@@ -52,7 +52,7 @@ class UserProfilesManager:
             approx_amount_top_n=5,
             filter_useful_topics=False
         ).get_build_parameters(),
-        # ProfileCreator(  # TODO: code fixen
+        # ProfileCreator(
         #     model_name="offline_bertopic_100000.bert",
         #     use_sentiment_in_scores=False,
         #     approx_mode=False,
@@ -60,7 +60,7 @@ class UserProfilesManager:
         #     approx_amount_top_n=5,
         #     filter_useful_topics=False
         # ).get_build_parameters(),
-        # ProfileCreator(  # TODO: te weinig RAM
+        # ProfileCreator(
         #     model_name="online_model_400top_97.bert",
         #     use_sentiment_in_scores=False,
         #     approx_mode=True,
@@ -68,7 +68,7 @@ class UserProfilesManager:
         #     approx_amount_top_n=10,
         #     filter_useful_topics=True
         # ).get_build_parameters(),
-        # ProfileCreator(  # TODO: cache missing
+        # ProfileCreator(
         #     model_name="online_model_400top_97.bert",
         #     use_sentiment_in_scores=False,
         #     approx_mode=True,
